2328a66fe5f4457026ac143a2cee375e395bf837
Yunus Emre <yemrecetin@outlook.com>
2026-07-06 12:25:59 +0300

    a lot of updates for flutter, native etc...

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import device_info_plus
 import platform_device_id
-import platform_device_id_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
 }
